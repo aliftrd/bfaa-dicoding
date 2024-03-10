a526be3f068028819558fc00d95d7577d4e3803c
Alif Triadi <aliftriadi.gaming@gmail.com>
2024-03-10 21:12:14 +0700

    feat: add setting feature
    add setting page and functionality

---
+++ app/src/androidTest/java/com/github/aliftrd/gitseeker/ui/SettingActivityTest.kt
@@ -0,0 +1,10 @@
+package com.github.aliftrd.gitseeker.ui
+
+import org.junit.jupiter.api.Assertions.*
+
+class SettingActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+}
---
+++ app/src/test/java/com/github/aliftrd/gitseeker/util/NumberTest.kt
@@ -0,0 +1,5 @@
+package com.github.aliftrd.gitseeker.util
+
+import org.junit.jupiter.api.Assertions.*
+
+class NumberTest
